1c3df322b00f97a0696fdbf169b8efa6aa2c43e3
Zoheb Hasan <106490884+ZohebHasan@users.noreply.github.com>
2024-01-28 11:45:42 +0000

    Build 2

--- src/hw0_main.c
+++ src/hw0_main.c
@@ -4,24 +4,32 @@
 //Declarations
 bool isValidSectionNum(int section);
 
+
 bool isValidSectionNum(int section){
-    if(section > 4 || section < 0){
+    if(section > 4 || section < 1){
         return false;
     }
     return true;
 }
 
 int main() {
+    int board[5] = {2, 2, 2, 2, 0};
     int section;
-    printf("Choose a section (1-4):_ ");
-    scanf("%d" , &section);
-    if(isValidSectionNum(section) == false){
-        while(isValidSectionNum(section) == false){
-            printf("Invalid choice. Choose a section (1-4):_ ");
-            scanf("%d" , &section);
+    for(int i = 0; i < sizeof(board); i++){
+        if(i <= 4){
+            printf("%d", board[i] + " ");
         }
+        printf("%d", " | " + board[i]);
         
     }
+    printf("Choose a section (1-4):_ ");
+    scanf("%d" , &section); 
+    while(isValidSectionNum(section) == false){
+        printf("Invalid choice. Choose a section (1-4):_ ");
+        scanf("%d" , &section);
+        }
+        
+    
 
     return 0;
 }
