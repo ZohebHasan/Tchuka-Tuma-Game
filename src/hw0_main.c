ae02dd1cc7ad0fd271430dad11b301e009704760
Zoheb Hasan <106490884+ZohebHasan@users.noreply.github.com>
2024-01-28 11:25:18 +0000

    Pre build

--- src/hw0_main.c
+++ src/hw0_main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+//Declarations
+bool isValidSectionNum(int section);
+
+bool isValidSectionNum(int section){
+    if(section > 4 || section < 0){
+        return false;
+    }
+    return true;
+}
 
 int main() {
-    // Your code here
-    printf("Hello world!\n");
+    int section;
+    printf("Choose a section (1-4):_ ");
+    scanf("%d" , &section);
+    if(isValidSectionNum(section) == false){
+        while(isValidSectionNum(section) == false){
+            printf("Invalid choice. Choose a section (1-4):_ ");
+            scanf("%d" , &section);
+        }
+        
+    }
+
     return 0;
 }
